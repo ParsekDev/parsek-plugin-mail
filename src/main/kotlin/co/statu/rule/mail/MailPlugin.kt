93baa6c5751ed0e979a0bfa34c4665eb639ace02
kahverengi <ahmet@duruer.dev>
2026-02-01 00:57:42 +0300

    feat: improved codebase

--- src/main/kotlin/co/statu/rule/mail/MailPlugin.kt
+++ src/main/kotlin/co/statu/rule/mail/MailPlugin.kt
@@ -1,10 +1,50 @@
 package co.statu.rule.mail
 
+import co.statu.parsek.PluginEventManager
 import co.statu.parsek.api.ParsekPlugin
 import co.statu.parsek.api.config.PluginConfigManager
+import co.statu.rule.database.DatabaseManager
+import co.statu.rule.mail.event.MailEventListener
+import co.statu.rule.token.provider.TokenProvider
+import io.vertx.ext.web.templ.handlebars.HandlebarsTemplateEngine
+import org.springframework.beans.factory.getBean
 
 class MailPlugin : ParsekPlugin() {
     companion object {
         internal lateinit var pluginConfigManager: PluginConfigManager<MailConfig>
+    }
+
+    override suspend fun onStart() {
+        val pluginConfigManager = PluginConfigManager(
+            this,
+            MailConfig::class.java,
+        )
+
+        MailPlugin.pluginConfigManager = pluginConfigManager
+
+        logger.info("Initialized plugin config")
+
+        val databaseManager = pluginGlobalBeanContext.beanFactory.getBean<DatabaseManager>()
+        val tokenProvider = pluginGlobalBeanContext.beanFactory.getBean<TokenProvider>()
+
+        val mailClientProvider =
+            MailClientProvider.create(vertx, pluginConfigManager)
+
+        val mailManager = MailManager(
+            pluginConfigManager,
+            HandlebarsTemplateEngine.create(vertx),
+            mailClientProvider,
+            databaseManager,
+            tokenProvider,
+            this
+        )
+
+        registerSingletonGlobal(mailManager)
+
+        val handlers = PluginEventManager.getEventListeners<MailEventListener>()
+
+        handlers.forEach {
+            it.onReady(mailManager)
+        }
     }
 }
