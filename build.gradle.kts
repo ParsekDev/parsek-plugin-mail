9ae71871422da093d4684cd0fca49220b817e849
kahverengi <ahmet@duruer.dev>
2026-01-31 21:25:39 +0300

    feat: use latest parsek core build

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
         compileOnly(project(mapOf("path" to ":plugins:parsek-plugin-database")))
         compileOnly(project(mapOf("path" to ":plugins:parsek-plugin-token")))
     } else {
-        compileOnly("dev.parsek:core:1.0.0-beta.18")
+        compileOnly("dev.parsek:core:1.0.0-beta.19")
         compileOnly("dev.parsek:parsek-plugin-database:1.0.0-dev.3")
         compileOnly("dev.parsek:parsek-plugin-token:1.0.0-dev.4")
     }
