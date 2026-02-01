aa40b891ff46194dc25d57897af928dac8f7452f
kahverengi <ahmet@duruer.dev>
2026-02-01 14:48:16 +0300

    chore: use local jar if there is no Parsek module

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,17 @@ repositories {
 
 dependencies {
     if (bootstrap) {
-        compileOnly(project(mapOf("path" to ":Parsek")))
-        compileOnly(project(mapOf("path" to ":plugins:parsek-plugin-database")))
-        compileOnly(project(mapOf("path" to ":plugins:parsek-plugin-token")))
+        listOf(
+            ":Parsek" to "Parsek*.jar",
+            ":plugins:parsek-plugin-database" to "parsek-plugin-database*.jar",
+            ":plugins:parsek-plugin-token" to "parsek-plugin-token*.jar"
+        ).forEach { (path, pattern) ->
+            if (findProject(path) != null) {
+                compileOnly(project(path))
+            } else {
+                compileOnly(fileTree(rootDir) { include(pattern) })
+            }
+        }
     } else {
         compileOnly("dev.parsek:core:1.0.0-beta.19")
         compileOnly("dev.parsek:parsek-plugin-database:1.0.0-dev.5")
